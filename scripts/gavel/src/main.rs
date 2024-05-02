0d386baf56d520e04315786ed1023b48ccf38743
hitchhooker <tommi@niemi.lol>
2024-05-03 05:07:37 +0700

    add --resolve feat into gavel's fetch

--- scripts/gavel/src/main.rs
+++ scripts/gavel/src/main.rs
@@ -1,9 +1,20 @@
+use std::net::{Ipv4Addr};
+
 use clap::{Parser, Subcommand};
 use serde_json::{json, Value};
 use tokio::main;
-use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
+use http::header::{HeaderValue, HOST};
+use tokio_tungstenite::{tungstenite::protocol::Message};
+use tokio_tungstenite::{connect_async, MaybeTlsStream};
+use tokio_tungstenite::tungstenite::client::IntoClientRequest;
 use futures_util::{StreamExt, SinkExt};
 use rand::{Rng, distributions::Alphanumeric};
+use url::Url;
+use native_tls::TlsConnector;
+use tokio_native_tls::TlsConnector as TokioTlsConnector;
+use std::net::{SocketAddr, IpAddr};
+use tokio::net::TcpStream;
+
 
 #[derive(Parser, Debug)]
 #[clap(version = "1.0", about = "Opinionated CLI tool to hammer the data out of blockchain via WebSockets.", long_about = None)]
@@ -22,6 +33,10 @@ enum Commands {
         /// The block number to fetch, which can be in decimal or hexadecimal format
         #[clap(short, long)]
         block_number: Option<String>,
+        
+        /// Custom resolve the endpoint with IP address
+        #[clap(short, long)]
+        resolve: Option<Ipv4Addr>,
     },
     /// Generate MMR proof for given block numbers
     Mmr {
@@ -38,8 +53,8 @@ enum Commands {
 async fn main() {
     let cli = Cli::parse();
     match cli.command {
-        Commands::Fetch { endpoint, block_number } => {
-            if let Err(e) = fetch_block(&endpoint, block_number.as_deref()).await {
+        Commands::Fetch { endpoint, block_number, resolve } => {
+            if let Err(e) = fetch_block(&endpoint, block_number.as_deref(), resolve.as_ref()).await {
                 eprintln!("Error: {}", e);
             }
         }
@@ -68,10 +83,60 @@ async fn identify_if_hexadecimal_or_decimal(block_number: Option<&str>) -> Resul
     }
 }
 
-async fn fetch_block(endpoint: &str, block_number: Option<&str>) -> Result<(), Box<dyn std::error::Error>> {
+/// Custom DNS resolution for WebSocket connection
+async fn custom_dns_connect(endpoint: &str, dns_override: Option<Ipv4Addr>) -> Result<tokio_tungstenite::WebSocketStream<tokio_tungstenite::MaybeTlsStream<tokio::net::TcpStream>>, Box<dyn std::error::Error>> {
+    let url = Url::parse(endpoint)?;
+    let addr = if let Some(ip) = dns_override {
+        SocketAddr::new(IpAddr::V4(ip), url.port_or_known_default().ok_or("Unknown port for the URL scheme")?)
+    } else {
+        // Fallback to DNS resolution if no override is provided
+        let host = url.host_str().ok_or("Missing host in URL")?;
+        format!("{}:{}", host, url.port_or_known_default().unwrap_or(443)).parse::<SocketAddr>()?
+    };
+
+    let tcp_stream = TcpStream::connect(addr).await?;
+    let tls_connector = TlsConnector::builder()
+        .danger_accept_invalid_certs(true) // this required for self-assgined addresses
+        .build()?;
+    let tokio_tls_connector = TokioTlsConnector::from(tls_connector);
+    let tls_stream = tokio_tls_connector.connect(url.host_str().unwrap_or(""), tcp_stream).await?;
+    let maybe_tls_stream = MaybeTlsStream::NativeTls(tls_stream);
+
+
+    let mut request = url.clone().into_client_request()?;
+    request.headers_mut().insert(HOST, HeaderValue::from_str(url.host_str().unwrap())?);
+
+    let (socket, _) = tokio_tungstenite::client_async(request, maybe_tls_stream).await?;
+    Ok(socket)
+}
+
+
+// async fn custom_dns_connect(endpoint: &str, dns_override: Option<Ipv4Addr>) -> Result<tokio_tungstenite::WebSocketStream<tokio_tungstenite::MaybeTlsStream<tokio::net::TcpStream>>, Box<dyn std::error::Error>> {
+//     let mut url = Url::parse(endpoint)?;
+//
+//     if let Some(ip) = dns_override {
+//         let port = url.port_or_known_default().ok_or("Unknown port for the URL scheme")?;
+//         url.set_host(Some(&format!("{}", ip))).unwrap();
+//         url.set_port(Some(port)).unwrap();
+//     }
+//
+//     let mut request = url.clone().into_client_request()?;
+//     request.headers_mut().insert(HOST, HeaderValue::from_str(url.host_str().unwrap())?);
+//
+//     let (socket, _) = connect_async(request).await?;
+//     Ok(socket)
+// }
+//
+async fn fetch_block(endpoint: &str, block_number: Option<&str>, ipv4: Option<&Ipv4Addr>) -> Result<(), Box<dyn std::error::Error>> {
     let formatted_block_number = identify_if_hexadecimal_or_decimal(block_number).await?;
 
-    let (mut socket, _) = connect_async(endpoint).await?;
+    let mut socket = if let Some(ip) = ipv4 {
+        let connection = custom_dns_connect(endpoint, Some(*ip)).await?;
+        connection
+    } else {
+        let (socket, _) = connect_async(endpoint).await?;
+        socket
+    };
 
     let method = if formatted_block_number.is_some() { "chain_getBlockHash" } else { "chain_getHead" };
     let params = json!([formatted_block_number]);
@@ -89,13 +154,15 @@ async fn fetch_block(endpoint: &str, block_number: Option<&str>) -> Result<(), B
 async fn get_mmr_proof(endpoint: &str, block_numbers: Vec<u64>) -> Result<(), Box<dyn std::error::Error>> {
     let (mut socket, _) = connect_async(endpoint).await?;
 
-    let params = json!([block_numbers]); // No need to be mutable
+    let params = json!([block_numbers]);
 
     let block_data = send_and_receive(&mut socket, "mmr_generateProof", params).await?;
 
     println!("{}", serde_json::to_string_pretty(&block_data)?);
     Ok(())
 }
+
+
 
 async fn send_and_receive(
     socket: &mut tokio_tungstenite::WebSocketStream<tokio_tungstenite::MaybeTlsStream<tokio::net::TcpStream>>,
