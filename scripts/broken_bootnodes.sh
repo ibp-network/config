03e5ede015189d536d72542147b88f6b5a052b42
hitchhooker <tommi@niemi.lol>
2024-05-03 02:07:50 +0700

    fix filter script

--- scripts/broken_bootnodes.sh
+++ scripts/broken_bootnodes.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 RESULTS_JSON="/tmp/bootnode_tests/results.json"
-jq '.[] | .[] | select(.valid == false)' "$RESULTS_JSON"
-
+jq 'map_values(
+      map_values(
+        select(.valid == false and (.network | contains("encointer") | not) and .peers == "0")
+      )
+    )' $RESULTS_JSON
